e69c8163bd819fa73a35186540782da45d31ff9c
Cristina Torres <ctorresinfo@gmail.com>
2014-07-11 18:30:21 +0200

    Boostrap HTML Welcome
    This will be the principal panel

---
+++ welcome.php
@@ -0,0 +1,44 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="es" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="es" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="es"> <!--<![endif]-->
+<head>
+    <title>Panel Principal</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Systamina">
+
+    <!-- CSS Global Compulsory-->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/sysstyle.css">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <!-- CSS Implementing Plugins -->
+
+    <!-- CSS Page Style -->
+
+    <!-- CSS Theme -->
+
+</head>
+
+<body>
+<!--=== Header ===-->
+<div class="navbar">
+</div>
+<!--=== End Header ===-->
+<div class="container">
+    <h1>Bienvenido</h1>
+    <hr />
+
+</div>
+</body>
+</html>
